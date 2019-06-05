a0768953203049d694d22c82a16a14bb22521f8f
Peter <peter@Peters-MBP-2.localdomain>
2019-06-05 15:41:56 -0400

    incomplete, added some args

--- echo.py
+++ echo.py
@@ -2,21 +2,30 @@
 # -*- coding: utf-8 -*-
 """An enhanced version of the 'echo' cmd line utility"""
 
-__author__ = "???"
+__author__ = "peter mayor"
 
 
 import sys
+import argparse
 
 
 def create_parser():
     """Creates and returns an argparse cmd line option parser"""
-    pass
+    parser = argparse.ArgumentParser()
+    # parser.add_argument('-h/--help', help="show this help message and exit")
+    parser.add_argument('-u','--upper', metavar='', help="convert text to uppercase")
+    parser.add_argument('-l','--lower', metavar='', help="convert text to lowercase")
+    parser.add_argument('-t','--title', metavar='', help="convert text to titlecase")
+    return parser.parse_args()
 
 
-def main(args):
+
+def main():
     """Implementation of echo"""
-    pass
+    args = create_parser()
+    # print args
+    
 
 
 if __name__ == '__main__':
-    pass
+    main()
--- tests/test_echo.py
+++ tests/test_echo.py
@@ -5,7 +5,18 @@ import unittest
 import echo
 
 # Your test case class goes here
+def test_help(self):
+    """ Running the program without arguments should show usage. """
 
+    # Run the command `python ./echo.py -h` in a separate process, then
+    # collect it's output.
+    process = subprocess.Popen(
+        ["python", "./echo.py", "-h"],
+        stdout=subprocess.PIPE)
+    stdout, _ = process.communicate()
+    usage = open("./USAGE", "r").read()
+
+    self.assertEquals(stdout, usage)
 
 if __name__ == '__main__':
     unittest.main()
