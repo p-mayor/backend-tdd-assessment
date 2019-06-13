77530fcd30a49935c4f1fde9b520452d650cbd5b
Peter <peter@MacBook-Pro.localdomain>
2019-06-13 15:47:03 -0400

    tests passing

--- echo.py
+++ echo.py
@@ -2,21 +2,54 @@
 # -*- coding: utf-8 -*-
 """An enhanced version of the 'echo' cmd line utility"""
 
-__author__ = "???"
+__author__ = "peter mayor with help from tyler"
 
-
-import sys
+import argparse
 
 
 def create_parser():
     """Creates and returns an argparse cmd line option parser"""
-    pass
+    parser = argparse.ArgumentParser(
+        description='Perform transformation on input text.')
+    parser.add_argument("-u", "--upper", help="convert text to uppercase",
+                        action="store_true")
+    parser.add_argument("-l", "--lower", help="convert text to lowercase",
+                        action="store_true")
+    parser.add_argument("-t", "--title", help="convert text to titlecase",
+                        action="store_true")
+    parser.add_argument("text", help="text to be manipulated")
+    return parser
 
 
-def main(args):
+def to_upper(text):
+    return text.upper()
+
+
+def to_lower(text):
+    return text.lower()
+
+
+def to_title(text):
+    return text.title()
+
+
+def main():
     """Implementation of echo"""
-    pass
+    # namespace = create_parser()
+    parser = create_parser()
+    args = parser.parse_args()
+
+    if args.upper:
+        output = to_upper(args.text)
+    if args.lower:
+        output = to_lower(args.text)
+    if args.title:
+        output = to_title(args.text)
+    if not args.upper and not args.lower and not args.title:
+        output = args.text
+
+    print output
 
 
 if __name__ == '__main__':
-    pass
+    main()
